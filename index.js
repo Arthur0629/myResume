d3211328083bbd46be800778ba25eb8d6a1f0d64
Jui-Chieh Hsiao <n9326774@qut.edu.au>
2019-11-21 21:37:45 +1000

    add answer record

--- index.js
+++ index.js
@@ -17,24 +17,38 @@ app.directive('ngEnter', function () {
 app.controller("appCtrl", function($scope) {  
     $scope.message="Calculator";
     $scope.answer= "press enter or the button";
+    $scope.arrayOfItems = [];
     $scope.submit = function(){
         if($scope.Num1 == null || $scope.Num2 == null)
         {
             $scope.answer = 0;
+            var item = new Object;
+            item.itemName=$scope.answer;
+            $scope.arrayOfItems.push(item);
+            
         }
         else
         {
             $scope.answer = $scope.Num1 * $scope.Num2
+            var item = new Object;
+            item.itemName=$scope.answer;
+            $scope.arrayOfItems.push(item);
         }
     }
     $scope.enterPress = function(){
         if($scope.Num1 == null || $scope.Num2 == null)
         {
             $scope.answer = 0;
+            var item = new Object;
+            item.itemName=$scope.answer;
+            $scope.arrayOfItems.push(item);
         }
         else
         {
             $scope.answer = $scope.Num1 * $scope.Num2
+            var item = new Object;
+            item.itemName=$scope.answer;
+            $scope.arrayOfItems.push(item);
         }
     }
 });
